556a7a28e0b7082b053cd8b4e4c1a92d4e6831c9
Orin <staler-fenix@mail.ru>
2018-04-13 08:44:08 +0300

    refactor

--- src/main/java/ru/orindev/BostonGeneTest/config/JpaConfig.java
+++ src/main/java/ru/orindev/BostonGeneTest/config/JpaConfig.java
@@ -52,16 +52,6 @@ public class JpaConfig {
         return em;
     }
 
-//    @Bean
-//    public DataSource dataSource(){
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-//        dataSource.setUrl("jdbc:mysql://localhost:3306/spring_jpa");
-//        dataSource.setUsername( "tutorialuser" );
-//        dataSource.setPassword( "tutorialmy5ql" );
-//        return dataSource;
-//    }
-
     @Bean
     public PlatformTransactionManager transactionManager(
             EntityManagerFactory emf){
--- src/test/java/ru/orindev/BostonGeneTest/UserServiceTests.java
+++ src/test/java/ru/orindev/BostonGeneTest/UserServiceTests.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestClientException;
 import ru.orindev.BostonGeneTest.model.User;
 
 import java.sql.Date;
